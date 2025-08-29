096892af22ec2cb758af6c0c7385b58f9f289e9e
Ali Hasanov <nan.fgbs@gmail.com>
2025-08-29 11:43:14 +0400

    First Commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './styles/style.css'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from '/vite.svg'
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
