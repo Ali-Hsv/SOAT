14c7b707e2becc70efb394798efb256d60123390
Ali Hasanov <nan.fgbs@gmail.com>
2025-08-30 11:30:52 +0400

    Header + 1/2 Main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import './styles/style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from '/vite.svg'
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          1: '#119C59',
+          2: '#14B86E',
+          3: '#94DD8B',
+        },
+        secondary: '#780116',
+        error: '#F01445',
+        dark: {
+          black_1: '#25282B',
+          black_2: '#52575C',
+          gray_1: '#A0A4A8',
+          gray_2: '#CACCCF',
+          white_1: '#F4F3F1',
+          white_2: '#FAF9F7',
+        }
+      },
+      fontFamily: {
+        lexend: ['Lexend'],
+        quicksand: ['Quicksand'],
+      }
+    },
   },
   plugins: [],
 }
