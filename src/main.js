0086b589a9318544fa3f9600dd9bac61f163842e
Ali Hasanov <nan.fgbs@gmail.com>
2025-08-30 19:33:24 +0400

    Why SOAT Section

--- src/main.js
+++ src/main.js
@@ -4,3 +4,32 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
+if(ScrollTrigger.isTouch !== 1){
+    ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+		smooth: 1.5,
+		effects: true,
+	});
+
+    let lastScroll = 0;
+
+    ScrollTrigger.create({
+      start: 0,
+      end: "max", // вся длина документа
+      onUpdate: (self) => {
+        let currentScroll = self.scroll();
+
+        if (currentScroll > lastScroll) {
+          // скроллим вниз
+          gsap.to("#head-div", { y: -100, duration: 0.4, ease: "power2.out" });
+        } else {
+          // скроллим вверх
+          gsap.to("#head-div", { y: 0, duration: 0.4, ease: "power2.out" });
+        }
+
+        lastScroll = currentScroll;
+      }
+    });
+
+}
